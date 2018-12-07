295a77441fa3cb3a4e274a41147b844e1559055e
André Bauer <monotek23@gmail.com>
2018-12-07 18:36:46 +0100

    rebuilding site Fr 7. Dez 18:36:46 CET 2018

--- .hugosource/build-github-page.sh
+++ .hugosource/build-github-page.sh
@@ -3,16 +3,16 @@
 # build hugo site and sync to github
 #
 
+HUGO_SOURCE_DIR=".hugosource"
 REPO_ROOT="$(git rev-parse --show-toplevel)"
-
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+# Build the page
+hugo
 
-# rsync
-rsync -av --delete --exclude=.hugosource --exclude=.git --exclude=.github "${REPO_ROOT}/public/" "${REPO_ROOT}"
+# rsync to github page dir (repo root)
+rsync -av --delete --exclude=.hugosource --exclude=.git --exclude=.github "${REPO_ROOT}/${HUGO_SOURCE_DIR}/public/" "${REPO_ROOT}"
 
 # Add changes to git.
 git add --all .
